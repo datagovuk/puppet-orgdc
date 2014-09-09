f6bd94cf65edc2b50ec7664491bc60ef3ad659cf
David Otton <david@otton.org>
2014-09-09 14:21:26 +0100

    Dendencies on fuseki and beluga::wget

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,13 +1,9 @@
 # == Class orgdc::install
 #
 class orgdc::install {
-  include orgdc::params
+  include fuseki
+  include beluga::wget
 
-  if !defined( Package['wget'] ) {
-    package { 'wget':
-      ensure  => present,
-    }
-  }
   if !defined( Package['curl'] ) {
     package { 'curl':
       ensure  => present,
