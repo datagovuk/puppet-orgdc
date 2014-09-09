b47da19c9145a9e499509f3b0e00f9ca26a54f13
David Otton <david@otton.org>
2014-09-09 14:26:33 +0100

    Remove un-needed curl package

--- manifests/install.pp
+++ manifests/install.pp
@@ -3,14 +3,4 @@
 class orgdc::install {
   include fuseki
   include beluga::wget
-
-  if !defined( Package['curl'] ) {
-    package { 'curl':
-      ensure  => present,
-    }
-  }
-
-  include fuseki
-  include elda
-
 }
