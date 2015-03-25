1f0676692628fbe8e157469ba275bbef29cf22f2
Noel Sharpe <noels@radnetwork.co.uk>
2015-03-25 11:19:30 +0000

    Pass correct data dir to fuseki.

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,7 +1,9 @@
 # == Class orgdc::install
 #
 class orgdc::install {
-  include fuseki
+  class {'fuseki':
+    fuseki_data_dir => '/var/lib/fuseki/databases/ORG-DB',
+  }
   include elda
   include referencetimeserver
   include beluga::wget
